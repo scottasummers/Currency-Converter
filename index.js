6f89fb961c4d7722733e99701d9a64ff4ca8a08c
Scott Summers <scott.summers@flydenver.com>
2026-02-14 15:51:55 -0700

    JavaScript added

---
+++ index.js
@@ -0,0 +1,25 @@
+const currencyFirstEl = document.getElementById("currency-first");
+const worthFirstEl = document.getElementById("worth-first");
+const currencySecondEl = document.getElementById("currency-second");
+const worthSecondEl = document.getElementById("worth-second");
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+updateRate();
+
+function updateRate() {
+  fetch(
+    `https://v6.exchangerate-api.com/v6/a05fc9b3fa6b864812bb2254/latest/${currencyFirstEl.value}`,
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.conversion_rates[currencySecondEl.value];
+      console.log(rate);
+      exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondEl.value}`;
+    });
+}
+
+currencyFirstEl.addEventListener("change", updateRate);
+
+currencySecondEl.addEventListener("change", updateRate);
+
+worthFirstEl.addEventListener("input", updateRate);
